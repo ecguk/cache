c91360dafea46f7678ec9c9daf7ee061919b7d4d
서유찬 <seoyc4823@naver.com>
2024-05-06 16:47:35 +0900

    Initial commit

---
+++ cache.h
@@ -0,0 +1,41 @@
+#ifndef CACHE_H
+#define CACHE_H
+
+#include <string>
+
+#define CACHE_SIZE 10
+
+class Cache {
+private:
+    struct Node {
+        std::string key;
+        int int_value;
+        double double_value;
+        Node* next;
+
+        Node(std::string k, int iv, double dv) : key(k), int_value(iv), double_value(dv), next(nullptr) {}
+    };
+
+    Node* head;
+
+public:
+  Cache();
+  ~Cache();
+  // int를 cache에 추가한다
+  void add(std::string key, int value);
+  // double을 cache에 추가한다
+  void add(std::string key, double value);
+  // key에 해당하는 value를 cache에서 가져온다
+  // 타입과 key가 동일한 원소가 없다면 false를 반환한다.
+  bool get(std::string key, int &value);
+  // key에 해당하는 value를 cache에서 가져온다.
+  // 타입과 key가 동일한 원소가 없다면 false를 반환한다.
+  bool get(std::string key, double &value);
+
+  // 디버그 및 채점 용: 연결 리스트를 문자열로 표현하여 반환한다
+  // 다음과 같이 표현된 문자열을 반환한다
+  // [key1: value1] -> [key2: value2] -> ... -> [keyN: valueN]
+  std::string toString();
+};
+
+#endif
